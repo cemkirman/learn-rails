1a0b158f83faa2b031d97e39a7f5a58664a4e456
Cem Kirman <cem@nevereversleep.se>
2014-01-12 02:41:07 -0500

    implement sending mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,8 @@ class ContactsController < ApplicationController
       # Save data to Google Drive
       @contact.update_spreadsheet
 
-      # TODO send message
+      # Send message
+      UserMailer.contact_email(@contact).deliver
       
       flash[:notice] = "Message sent from #{@contact.name}"
       redirect_to root_path  
